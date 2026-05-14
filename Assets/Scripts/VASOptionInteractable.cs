09123973a18d02aabf222281ca91efc3ce8887a7
MonicaArcilaa <informationalllll@gmail.com>
2026-05-14 17:02:40 -0500

    Aplicar interacción con Raycasts en Menú y encuesta VAS

--- Assets/Scripts/VASOptionInteractable.cs
+++ Assets/Scripts/VASOptionInteractable.cs
@@ -7,12 +7,6 @@ public class VASButtonOption : MonoBehaviour, IPointerEnterHandler, IPointerExit
     [SerializeField] private int score;
     [SerializeField] private GameObject glowVisual;
 
-    [Header("Activación por toque")]
-    [SerializeField] private string handTag = "Hand";
-    [SerializeField] private float activacionCooldown = 0.2f;
-
-    private float ultimoTiempoActivacion = -999f;
-
     public void Seleccionar()
     {
         if (encuestaManager == null)
@@ -21,10 +15,6 @@ public class VASButtonOption : MonoBehaviour, IPointerEnterHandler, IPointerExit
         if (encuestaManager.RespuestaEnviada)
             return;
 
-        if (Time.time - ultimoTiempoActivacion < activacionCooldown)
-            return;
-
-        ultimoTiempoActivacion = Time.time;
         encuestaManager.RegistrarRespuesta(score, glowVisual);
     }
 
@@ -40,46 +30,6 @@ public class VASButtonOption : MonoBehaviour, IPointerEnterHandler, IPointerExit
     public void OnPointerExit(PointerEventData eventData)
     {
         if (encuestaManager != null && encuestaManager.RespuestaEnviada)
-            return;
-
-        if (glowVisual != null)
-            glowVisual.SetActive(false);
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (encuestaManager != null && encuestaManager.RespuestaEnviada)
-            return;
-
-        if (!other.CompareTag(handTag))
-            return;
-
-        if (glowVisual != null)
-            glowVisual.SetActive(true);
-
-        Seleccionar();
-    }
-
-    private void OnTriggerStay(Collider other)
-    {
-        if (encuestaManager != null && encuestaManager.RespuestaEnviada)
-            return;
-
-        if (!other.CompareTag(handTag))
-            return;
-
-        if (glowVisual != null)
-            glowVisual.SetActive(true);
-
-        Seleccionar();
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (encuestaManager != null && encuestaManager.RespuestaEnviada)
-            return;
-
-        if (!other.CompareTag(handTag))
             return;
 
         if (glowVisual != null)
